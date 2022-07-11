3c1c2cd955a1d7e05cd91acf48e54d2aa747446b
ralphkw1 <ralphkw1@gmail.com>
2022-07-11 17:28:01 -0500

    Rework use of MessageDialog.DoNotAskOption for platform 213 deprecation.

--- src/main/java/net/ddns/rkdawenterprises/brief4ijidea/compatibility/Compatibility.kt
+++ src/main/java/net/ddns/rkdawenterprises/brief4ijidea/compatibility/Compatibility.kt
@@ -28,9 +28,17 @@ package net.ddns.rkdawenterprises.brief4ijidea.compatibility
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.ex.ActionUtil
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.EditorCoreUtil.isTrueSmoothScrollingEnabled
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.openapi.ui.Messages
+import com.intellij.util.ui.UIUtil
+import net.ddns.rkdawenterprises.brief4ijidea.Localized_messages
+import net.ddns.rkdawenterprises.brief4ijidea.State_component
 import java.awt.Rectangle
+import javax.swing.Icon
 
 fun get_editor_content_visible_area(editor: Editor): Rectangle
 {
@@ -44,4 +52,129 @@ fun perform_action_dumb_aware_with_callbacks(an_action: AnAction,
     ActionUtil.performActionDumbAwareWithCallbacks(an_action,
                                                    an_action_event,
                                                    an_action_event.dataContext);
+}
+
+fun virtual_space_setting_warning(editor: Editor)
+{
+    val do_not_show_virtual_space_setting_dialog = State_component.get_instance()
+        .get_do_not_show_virtual_space_setting_dialog();
+    if(!do_not_show_virtual_space_setting_dialog)
+    {
+        val editor_settings = editor.settings;
+        if(!editor_settings.isVirtualSpace)
+        {
+            ApplicationManager.getApplication()
+                .invokeLater {
+                    warning_message("Change Settings for this Command",
+                                    Localized_messages.message("you.must.enable.settings.editor.general.virtual.space.after.the.end.of.line.for.some.commands.right.side.of.window.and.column.marking.mode.to.work.properly"),
+                                    object : DialogWrapper.DoNotAskOption.Adapter()
+                                    {
+                                        /**
+                                         * Save the state of the checkbox in the settings, or perform some other related action.
+                                         * This method is called right after the dialog is [closed][.close].
+                                         * <br></br>
+                                         * Note that this method won't be called in the case when the dialog is closed by [Cancel][.CANCEL_EXIT_CODE]
+                                         * if [saving the choice on cancel is disabled][.shouldSaveOptionsOnCancel] (which is by default).
+                                         *
+                                         * @param isSelected true if user selected "don't show again".
+                                         * @param exitCode   the [exit code][.getExitCode] of the dialog.
+                                         * @see .shouldSaveOptionsOnCancel
+                                         */
+                                        /**
+                                         * Save the state of the checkbox in the settings, or perform some other related action.
+                                         * This method is called right after the dialog is [closed][.close].
+                                         * <br></br>
+                                         * Note that this method won't be called in the case when the dialog is closed by [Cancel][.CANCEL_EXIT_CODE]
+                                         * if [saving the choice on cancel is disabled][.shouldSaveOptionsOnCancel] (which is by default).
+                                         *
+                                         * @param isSelected true if user selected "don't show again".
+                                         * @param exitCode   the [exit code][.getExitCode] of the dialog.
+                                         * @see .shouldSaveOptionsOnCancel
+                                         */
+                                        override fun rememberChoice(isSelected: Boolean,
+                                                                    exitCode: Int)
+                                        {
+                                            State_component.get_instance()._do_not_show_virtual_space_setting_dialog = isSelected;
+                                        }
+                                    });
+                }
+        }
+    }
+}
+
+fun warning_message(title: String?,
+                    message: String,
+                    option: DialogWrapper.DoNotAskOption.Adapter? = null): String?
+{
+    val button_name = "OK";
+
+    return Message(title,
+                   message)
+        .buttons(button_name)
+        .default_button(button_name)
+        .focused_button(button_name)
+        .do_not_ask(option)
+        .as_warning()
+        .show();
+}
+
+class Message internal constructor(private val title: String?,
+                                   private val message: String)
+{
+    private var m_icon: Icon? = null;
+    private var m_do_not_ask_option: DialogWrapper.DoNotAskOption? = null;
+    private lateinit var m_buttons: List<String>;
+    private var m_default_button_name: String? = null;
+    private var m_focused_button_name: String? = null;
+
+    fun icon(icon: Icon?): Message
+    {
+        m_icon = icon;
+        return this;
+    }
+
+    fun as_warning(): Message
+    {
+        m_icon = UIUtil.getWarningIcon();
+        return this;
+    }
+
+    fun do_not_ask(option: DialogWrapper.DoNotAskOption.Adapter?): Message
+    {
+        m_do_not_ask_option = option;
+        return this;
+    }
+
+    fun buttons(vararg button_names: String): Message
+    {
+        m_buttons = button_names.toList();
+        return this;
+    }
+
+    fun default_button(default_button_name: String): Message
+    {
+        m_default_button_name = default_button_name;
+        return this;
+    }
+
+    fun focused_button(focused_button_name: String): Message
+    {
+        m_focused_button_name = focused_button_name;
+        return this;
+    }
+
+    fun show(project: Project? = null): String?
+    {
+        val options = m_buttons.toTypedArray();
+        val default_option_index = m_buttons.indexOf(m_default_button_name);
+        val result = Messages.showIdeaMessageDialog(project,
+                                                    message,
+                                                    title,
+                                                    options,
+                                                    default_option_index,
+                                                    m_icon,
+                                                    m_do_not_ask_option);
+
+        return if(result < 0) null else m_buttons[result];
+    }
 }
